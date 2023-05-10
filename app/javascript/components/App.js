be5ea6eb7dec34b0231fd3443edb484947a70793
Bludidi <bludidi5@gmail.com>
2023-05-10 21:04:37 +0200

    Add router

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const App = () => {
+  <>
+    <h3>Greeting:</h3>
+    <Link to='/greeting'>Greetings</Link>
+  </>
+}
+
+export default App;
